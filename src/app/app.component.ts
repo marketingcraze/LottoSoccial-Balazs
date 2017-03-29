382eed26e5a1ed99d5c716bbb41c668dc659dd14
Md. Shahriar Kabir <shahriar040@gmail.com>
2017-03-29 21:12:58 +0600

    Home card offer time style fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,8 +20,6 @@ export class MyApp {
         alertCtrl:AlertController) {
     
     platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       // StatusBar.styleDefault();
       StatusBar.hide();
 
