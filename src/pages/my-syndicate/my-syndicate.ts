d5d1c4bd94b0f623bc07a5676362ea3d6c1ad93f
Md. Shahriar Kabir <shahriar040@gmail.com>
2017-06-09 01:27:25 +0600

    App syndicate merged

--- src/pages/my-syndicate/my-syndicate.ts
+++ src/pages/my-syndicate/my-syndicate.ts
@@ -3,42 +3,53 @@ import { NavController, NavParams, App, Tabs } from 'ionic-angular';
 import { ManageSyndicatePage } from '../manage-syndicate/manage-syndicate';
 import { ManageSyndicate2Page } from '../manage-syndicate2/manage-syndicate2';
 import { YourTicketsPage } from '../your-tickets/your-tickets';
-//import { SyndicatePage } from '../syndicate/syndicate';
-declare var $: any;
-/*
-  Generated class for the MySyndicate page.
+import { SyndicateService } from '../../providers/syndicate-service';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
+
+declare var $: any;
+
 @Component({
-  selector: 'page-my-syndicate',
-  templateUrl: 'my-syndicate.html'
+    selector: 'page-my-syndicate',
+    templateUrl: 'my-syndicate.html'
 })
 export class MySyndicatePage {
+    private syndArr = [];
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public app: App,
+        public _syndService: SyndicateService) { }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public app: App) {}
-
-  ionViewDidLoad() {
-    $('#estate').hide();
-    $('#sstate').show();
-  }
-  ionViewWillEnter() {
-    $('#estate').hide();
-    $('#sstate').show();
-  }
-  checkwins() {
-    var t: Tabs = this.navCtrl.parent;
-    t.select(1);
-  }
-  manage_syndicates() {
-    this.app.getRootNav().push(ManageSyndicatePage);
-  }
-  manage_syndicates2() {
-    this.app.getRootNav().push(ManageSyndicate2Page);
-  }
-  viewTickets() {
-    this.app.getRootNav().push(YourTicketsPage);
-  }
+    ionViewDidLoad() {
+        this.loadSyndicate();
+        $('#estate').hide();
+        $('#sstate').show();
+    }
+    ionViewWillEnter() {
+        $('#estate').hide();
+        $('#sstate').show();
+    }
+    checkwins() {
+        var t: Tabs = this.navCtrl.parent;
+        t.select(1);
+    }
+    manage_syndicates() {
+        this.app.getRootNav().push(ManageSyndicatePage);
+    }
+    manage_syndicates2() {
+        this.app.getRootNav().push(ManageSyndicate2Page);
+    }
+    viewTickets() {
+        this.app.getRootNav().push(YourTicketsPage);
+    }
+    loadSyndicate() {
+        this._syndService.syndicateList().subscribe((res) => {
+            console.log('syndicate list');
+            if (res.response.response.status == 'SUCCESS') {
+                this.syndArr = res.response.response.syndicate_group;
+            }
+            console.log(this.syndArr);
+        })
+    }
 
 }
--- src/providers/syndicate-service.ts
+++ src/providers/syndicate-service.ts
@@ -2,76 +2,60 @@ import { Injectable } from '@angular/core';
 import { Http, RequestOptions, Headers, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { DomSanitizer } from '@angular/platform-browser';
-import { Observable, ObservableInput } from 'rxjs/Observable';
-
-import { CommonService } from '../services/common.service';
-import { Params } from '../services/params';
+//import { Observable, ObservableInput } from 'rxjs/Observable';
 
 @Injectable()
 export class SyndicateService {
-    apiUrl:string = 'https://nima.lottosocial.com/wp-json/mobi/v2/' ;
+    apiUrl: string = 'https://nima.lottosocial.com/wp-json/mobi/v2/';
 
-    constructor(
-        private http:Http,
-        private params:Params,
-        private sanitizer: DomSanitizer, 
-        public file: File ) {}
-  
+    constructor(private http: Http, private sanitizer: DomSanitizer, public file: File) { }
+
     static get parameters() {
         return [[Http]];
     }
 
-    public static getHeader(): Headers{
+    public static getHeader(): Headers {
         let myHeaders: Headers = new Headers();
 
         myHeaders.set('Content-type', 'application/json');
 
         myHeaders.append('Authorization', 'Oauth oauth_consumer_key = "NDes1FKC0Kkg",' +
-        'oauth_signature_method="HMAC-SHA1",' +
-        'oauth_timestamp="1490087533",' +
-        'oauth_nonce="dWL9pr",' +
-        'oauth_version="1.0",' +
-        'oauth_token="djKnEJjJ7TYw0VJEsxGEtlfg",' +
-        'oauth_signature="mQF41gSF7KIuVqzqcI0nSX1UklE%3D"');
-        
+            'oauth_signature_method="HMAC-SHA1",' +
+            'oauth_timestamp="1490087533",' +
+            'oauth_nonce="dWL9pr",' +
+            'oauth_version="1.0",' +
+            'oauth_token="djKnEJjJ7TYw0VJEsxGEtlfg",' +
+            'oauth_signature="mQF41gSF7KIuVqzqcI0nSX1UklE%3D"');
+
         return myHeaders;
     }
 
-    getcovers(){
-            if (!CommonService.isOnline) {
-            this.params.setIsInternetAvailable(false);
-            return;
-        }
+    getcovers() {
         let action = 'privatesyndicate/';
-            let data =  {
-                          "session_ID":"avjtjgu0f257f0orggqufcn5g2",
-                          "page_ID": "4",
-                          "screen_id": "4.1",
-                          "action": "ilist_banner",
-                          "website": "Lotto Social",
-                          "website_id": "27",
-                          "source_site": "mobi.lottosocial.com",
-                          "module_name": "get_syndicate_images_lib",
-                          "customer_id": "1970400"  
-                        }
-            let headopt = SyndicateService.getHeader();
-            return this.http.post(this.apiUrl + action, data, { headers:headopt })
+        let data = {
+            "session_ID": "avjtjgu0f257f0orggqufcn5g2",
+            "page_ID": "4",
+            "screen_id": "4.1",
+            "action": "ilist_banner",
+            "website": "Lotto Social",
+            "website_id": "27",
+            "source_site": "mobi.lottosocial.com",
+            "module_name": "get_syndicate_images_lib",
+            "customer_id": "1970400"
+        }
+        let headopt = SyndicateService.getHeader();
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
             .map(res => res.json())
             .map((res) => {
                 return res;
             })
-            
+
     }
 
-    uploadCover(filePath: any){
+    uploadCover(filePath: any) {
 
-      if (!CommonService.isOnline) {
-            this.params.setIsInternetAvailable(false);
-            return;
-        }
-        
-       let myHeaders: Headers = new Headers();
-        myHeaders.set('Authorization', 
+        let myHeaders: Headers = new Headers();
+        myHeaders.set('Authorization',
             'Oauth oauth_consumer_key = "NDes1FKC0Kkg",' +
             'oauth_token="djKnEJjJ7TYw0VJEsxGEtlfg",' +
             'oauth_signature_method="HMAC-SHA1",' +
@@ -79,70 +63,65 @@ export class SyndicateService {
             'oauth_nonce="dWL9pr",' +
             'oauth_version="1.0",' +
             'oauth_signature="mQF41gSF7KIuVqzqcI0nSX1UklE%3D"'
-            );
+        );
         myHeaders.append('Content-type', 'multipart/form-data');
 
-    // var options = {
-    //   fileKey: "file",
-    //   fileName: 'img'+new Date().getTime(),
-    //   chunkedMode: false,
-    //   mimeType: "multipart/form-data",
-    //   params : {
-    //     'fileName':'img'+new Date().getTime()
-    //   }
-    // };
+        // var options = {
+        //   fileKey: "file",
+        //   fileName: 'img'+new Date().getTime(),
+        //   chunkedMode: false,
+        //   mimeType: "multipart/form-data",
+        //   params : {
+        //     'fileName':'img'+new Date().getTime()
+        //   }
+        // };
 
-    var extension = filePath.substr(filePath.lastIndexOf('.') + 1);
+        var extension = filePath.substr(filePath.lastIndexOf('.') + 1);
         let options = {
             fileKey: 'file',
-           //fileName: 'name.jpg',
-           chunkedMode: false,
+            //fileName: 'name.jpg',
+            chunkedMode: false,
             mimeType: "multipart/form-data",
             headers: myHeaders
         }
 
 
-     // console.log('inside service upload');
-      
-      // console.log(imgdata)
-      console.log('inside upload service');
-      console.log(extension)      
+        // console.log('inside service upload');
+
+        // console.log(imgdata)
+        console.log('inside upload service');
+        console.log(extension)
         console.log(filePath)
-      var apiUrl = 'https://nima.lottosocial.com/wp-json/mobi/v2/upload/?process=syndicate';
-      // let headopt = SyndicateService.getHeader();
-     //  let formData:FormData = new FormData();
+        var apiUrl = 'https://nima.lottosocial.com/wp-json/mobi/v2/upload/?process=syndicate';
+        // let headopt = SyndicateService.getHeader();
+        //  let formData:FormData = new FormData();
 
         let formData = new FormData();
-       formData.append('image', filePath);
-       console.log('formdata',apiUrl,options);
-       console.log(formData);
-      return this.http.post(apiUrl, formData, options)
-      .map(res => res.json())
-      .map((res) => {
-        return res;
-      })
+        formData.append('image', filePath);
+        console.log('formdata', apiUrl, options);
+        console.log(formData);
+        return this.http.post(apiUrl, formData, options)
+            .map(res => res.json())
+            .map((res) => {
+                return res;
+            })
     }
 
     getLotteries() {
-      if (!CommonService.isOnline) {
-            this.params.setIsInternetAvailable(false);
-            return;
-        }
-        
         let action = 'privatesyndicate'
-        let data =  {
-                          "session_ID":"avjtjgu0f257f0orggqufcn5g2",
-                          "page_ID": "4",
-                          "screen_id": "4.3",
-                          "action": "syndicate_lotteries",
-                          "website": "Lotto Social",
-                          "website_id": "27",
-                          "source_site": "mobi.lottosocial.com",
-                          "module_name": "get_syndicate_lotteries",
-                          "customer_id": "1970400"  
-                        }
-            let headopt = SyndicateService.getHeader();
-            return this.http.post(this.apiUrl + action, data, { headers:headopt })
+        let data = {
+            "session_ID": "avjtjgu0f257f0orggqufcn5g2",
+            "page_ID": "4",
+            "screen_id": "4.3",
+            "action": "syndicate_lotteries",
+            "website": "Lotto Social",
+            "website_id": "27",
+            "source_site": "mobi.lottosocial.com",
+            "module_name": "get_syndicate_lotteries",
+            "customer_id": "1970400"
+        }
+        let headopt = SyndicateService.getHeader();
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
             .map(res => res.json())
             .map((res) => {
                 return res;
@@ -151,28 +130,28 @@ export class SyndicateService {
     createSynd(data: any) {
         let action = "privatesyndicate";
         let headopt = SyndicateService.getHeader();
-            return this.http.post(this.apiUrl + action, data, { headers:headopt })
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
             .map(res => res.json())
             .map((res) => {
                 return res;
             })
     }
     getTerms(id: any) {
-    let action = "privatesyndicate";
-    let headopt = SyndicateService.getHeader();
-    var data = {
-      "session_ID": "avjtjgu0f257f0orggqufcn5g2",
-      "page_ID": "4",
-      "screen_id": "4.5",
-      "action": "syndicate_terms",
-      "website": "Lotto Social",
-      "website_id": "27",
-      "source_site": "mobi.lottosocial.com",
-      "module_name": "get_private_syndicate_terms",
-      "customer_id":"1970400",
-      "private_syndicate_id":id
-    }
-    return this.http.post(this.apiUrl + action, data, { headers:headopt })
+        let action = "privatesyndicate";
+        let headopt = SyndicateService.getHeader();
+        var data = {
+            "session_ID": "avjtjgu0f257f0orggqufcn5g2",
+            "page_ID": "4",
+            "screen_id": "4.5",
+            "action": "syndicate_terms",
+            "website": "Lotto Social",
+            "website_id": "27",
+            "source_site": "mobi.lottosocial.com",
+            "module_name": "get_private_syndicate_terms",
+            "customer_id": "1970400",
+            "private_syndicate_id": id
+        }
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
             .map(res => res.json())
             .map((res) => {
                 return res;
@@ -181,12 +160,6 @@ export class SyndicateService {
     }
 
     syndnumber(id: any) {
-        if (!CommonService.isOnline) {
-            this.params.setIsInternetAvailable(false);
-            return;
-        }
-        
-
         let action = "privatesyndicate";
         let headopt = SyndicateService.getHeader();
         var data = {
@@ -198,23 +171,16 @@ export class SyndicateService {
             "website_id": "27",
             "source_site": "mobi.lottosocial.com",
             "module_name": "get_private_syndicate_details",
-            "customer_id":"1970400",
-            "private_syndicate_id":id
-    }
-    return this.http.post(this.apiUrl + action, data, { headers:headopt })
+            "customer_id": "1970400",
+            "private_syndicate_id": id
+        }
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
             .map(res => res.json())
             .map((res) => {
                 return res;
             })
     }
-
-
     getBigJack(id: any) {
-        if (!CommonService.isOnline) {
-            this.params.setIsInternetAvailable(false);
-            return;
-        }
-        
         let action = "privatesyndicate";
         let headopt = SyndicateService.getHeader();
         var data = {
@@ -226,37 +192,48 @@ export class SyndicateService {
             "website_id": "27",
             "source_site": "mobi.lottosocial.com",
             "module_name": "get_big_jackpot_list",
-            "customer_id":"1970400",
-            "private_syndicate_id":id
-    }
-    return this.http.post(this.apiUrl + action, data, { headers:headopt })
+            "customer_id": "1970400",
+            "private_syndicate_id": id
+        }
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
             .map(res => res.json())
             .map((res) => {
                 return res;
             })
     }
-
-
     buySyndicate(data: any) {
-        if (!CommonService.isOnline) {
-            this.params.setIsInternetAvailable(false);
-            return;
-        }
-        
         let action = "privatesyndicate";
         let headopt = SyndicateService.getHeader();
-    return this.http.post(this.apiUrl + action, data, { headers:headopt })
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
             .map(res => res.json())
             .map((res) => {
                 return res;
             })
+    }
+
+    syndicateList() {
+        let action = "privatesyndicate";
+        let headopt = SyndicateService.getHeader();
+        var data = {
+            "session_ID": "avjtjgu0f257f0orggqufcn5g2",
+            "page_ID": "6",
+            "screen_id": "6.1",
+            "action": "syndicate_list",
+            "website": "Lotto Social",
+            "website_id": "27",
+            "source_site": "mobi.lottosocial.com",
+            "module_name": "get_syndicate_list",
+            "customer_id": "1970400"
+        }
+        return this.http.post(this.apiUrl + action, data, { headers: headopt })
+            .map(res => res.json())
+            .map((res) => {
+                return res;
+            })
+
     }
 
 }
-
-
-
-
 
 
 
