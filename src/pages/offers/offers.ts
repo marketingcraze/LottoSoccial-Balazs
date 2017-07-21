05234f85453d2bec3cc3e9abd8ef8b4956b4480c
harshad <introin@gmail.com>
2017-07-21 18:00:39 +0530

    offer page buy now button functioanlity

--- src/pages/offers/offers.ts
+++ src/pages/offers/offers.ts
@@ -1,217 +1,119 @@
-import { Component, ViewChild } from '@angular/core';
-import { Platform, NavController, NavParams, LoadingController } from 'ionic-angular';
-
-import { InAppBrowser } from '@ionic-native/in-app-browser';
-
-import { CommonService } from '../../services/common.service';
+import { Component } from '@angular/core';
+import { Platform,NavController, NavParams,LoadingController } from 'ionic-angular';
 import { AuthService } from '../../services/auth.service';
-import { OfferService } from '../../services/offer.service';
-import { Params } from '../../services/params';
+import { FilterPipe } from '../../pipes/filter-pipe';
 
-import { AppSoundProvider } from '../../providers/app-sound/app-sound';
 
+
+/*
+  Generated class for the Offers page.
+  See http://ionicframework.com/docs/v2/components/#navigation for more info on
+  Ionic pages and navigation.
+*/
 @Component({
   selector: 'page-offers',
   templateUrl: 'offers.html',
+
 })
-export class OffersPage {
-  @ViewChild("confirmPayment") confirmPayment;
-
-  toptab: string = "offer";
-
-  userCards: any;
-  userCardsCount:number = 0;
-  customerToken:string;
-  jackpotList:any
-  jackpotGroup:any
 
 
-  credit_lines: any;
-  credit_offer: any;
-  credit_filter_line: any;
-  credit_filter_draw: any;
-  credit_filter_day: any;
+export class OffersPage   {
+  toptab:string="offer";
+  credit_filter_line:any;
+  credit_filter_draw:any;
+  credit_filter_day:any;
+  credit_offer : any;
+  credit_product:any;
+  credit_game:any;
+  Credit_Points:any;
+  buyoffer:any;
+  private loading : any;
+  resultshow:boolean=false;
+  errorshow:boolean=false;
+  slider:any;
+  productdata:any;
 
-  fetch_lines: any;
-  fetch_offer: any;
-  fetch_filter_line: any;
-  fetch_filter_draw: any;
-  fetch_filter_day: any;
-    drawdaytue:any ="#2F76F1";
-    drawdayfri:any= "#AAAAAA";
-    drawdaywed:any ="#FF0000";
-    drawdaysat:any= "#AAAAAA";
-  Credit_Points: any;
-
-spaceBetween:number ;
-
-  constructor(
-    public platform: Platform,
-    public params: Params,
-    public iab: InAppBrowser,
-    public navCtrl: NavController,
-    public navParams: NavParams,
-    public authSrv: AuthService,
-    public srvOffer: OfferService,
-    public appSound:AppSoundProvider,
-    public loadingCtrl: LoadingController) {
-
-        this.spaceBetween = Math.floor(platform.width() * -0.22);
-
-    //   this.spaceBetween = Math.floor( platform.width() * -0.14 );
-      this.checkCardExists();
+  constructor(public navCtrl: NavController,private loadingCtrl: LoadingController,  public platform: Platform,public navParams: NavParams,public authSrv:AuthService ) {
+      //  this.spaceBetween = Math.floor(platform.width() * -0.22);
   }
-
-  checkCardExists(){
-    console.log("OffersPage::checkCardExists()");
-    let loader = this._showLoader();
+     // draw day click  call this function     
+  drawday(day,gamename){
+    switch (day) {
+      case "Mon" :  this.credit_filter_day="Monday";     break;
+      case "Tue" :  this.credit_filter_day="Tuesday";    break;
+      case "Wed" :  this.credit_filter_day="Wednesday";  break;
+      case "Thu" :  this.credit_filter_day="Thursday";   break;
+      case "Fri" :  this.credit_filter_day="Friday";     break;
+      case "Sat" :  this.credit_filter_day="Saturday";   break;
+      case "Sun" :  this.credit_filter_day="Sunday";     break;
+      default :     this.credit_filter_day="Not selected";
+    }
+    this.credit_filter_draw=day;
+    var string = gamename;
+    this.credit_game= string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  // line select  call this function   
+  credit_line(line){
+      this.credit_filter_line=parseInt(line);
+  }
+   
+   
+  ionViewWillEnter() {
+  // get creaditoffer call api
+      this.authSrv.get_credit_offer().subscribe(data=>{
     
-    this.srvOffer.getJackpotList().subscribe((data) => {
-      console.log("OffersPage::getJackpotList() success", data);
-      if (data.response && data.response[0] 
-        && data.response[0].get_big_jackpot_list) {
-        this.jackpotList = data.response[0].get_big_jackpot_list.response;
-        this.customerToken = this.jackpotList.customer_token;
-      }
-
-      loader.dismiss();
+          this.credit_offer=data.response.response.offers;
+          this.credit_product=data.response.response.product;
+          console.log("get_credit_offer",data);
+        },
+        err=>{
+           console.log("error", err);
+          },
+        ()=> console.log("offer dislpay sucesss")
+      );
       
-    }, (err) => {
-      console.log("OffersPage::getJackpotList() error", err);
-      loader.dismiss();
+
+  // get creditpoints call api 
+      this.authSrv.get_Credit_Points().subscribe(data=>{
+          this.Credit_Points=data.response.response.bonus_credit;
+          //console.log("get_Credit_Points",data)
+        },
+        err=>{     
+          console.log("error", err);
+        },
+        ()=> console.log("creadit points get successfully")
+      );
+  }
+ 
+  // buy buton click call this function
+  buyCreditOffer(){
+     this.loading = this.loadingCtrl.create();
+    this.loading.present().then(() => {
+      this.authSrv.buy_Credit_Offer().subscribe(data=>{
+        this.loading.dismiss();
+        this.resultshow=true;
+          this.buyoffer=data;
+          console.log(this.buyoffer);    
+        },
+        err=>{   
+          this.errorshow=true;
+          console.log("error", err);
+        },
+        ()=> console.log("offer buy successfully")
+      );
     })
   }
-
-  showPaymentOptions() {
-    console.log("OffersPage::showPaymentOptions()");
-    let offer = {total_cost:4.99} ;
-
-    this.appSound.play('buttonClick');
-    if (this.customerToken) {
-      this.goPaymentWebview();
-    }else{
-      let loader = this._showLoader();
-      // get all the cards details
-      this.srvOffer.getExistingPaymilCardsDetails().subscribe((data) => {
-        console.log("OffersPage::showPaymentOptions() success", data);
-        let token_exists = 0;
-        for (var i = 0; i < data.response.length; ++i) {
-          if (data.response[i].get_customer_paymill_card_details) {
-            token_exists = data.response[i].get_customer_paymill_card_details.response.token_exists
-          } 
-        }
-
-        if (token_exists > 0) {
-          data.response.push({ offer: offer });
-          this.userCards = data.response;
-          console.log("OffersPage::showPaymentOptions() success", this.userCards);
-          loader.dismiss();
-          this.confirmPayment.togglePopup()
-        }else{
-          this.goPaymentWebview();
-        }
-
-      }, (err) => {
-        console.log("OffersPage::showPaymentOptions() error", err);
-        loader.dismiss();
-      })
-    }
+  getmoreline(){
+    this.resultshow=false;
   }
-  tabChanged(){
-    // console.log("OffersPage::tabChanged()");
-    this.appSound.play('menuClick');
+  tryagain(){
+    this.errorshow=false;
   }
-
-  goPaymentWebview(){
-      let opt:string = "toolbarposition=top";
-      let str = 'https://nima.lottosocial.com/webview-auth/?redirect_to=free_reg'
-      str += '&customer_id='+CommonService.session.customer_id+'&customer_token='+this.customerToken+'&Offer_ID=1188'
-      this.iab.create( str, 'blank', opt);
-  }
-
-  private _showLoader() {
-    let loader = this.loadingCtrl.create({
-      content: "Loading data..."
-    });
-    loader.present()
-    return loader;
-  }
-
-
-    wed(drawday){
-        this.fetch_filter_draw=drawday;
-        this.fetch_filter_day="Wednesday";
-        this.drawdaywed="#FF0000";
-        this.drawdaysat="#AAAAAA";   
-    }
-    tue(drawday){
-        this.credit_filter_draw="Tue";
-        console.log();
-        this.credit_filter_day="Tuesday";
-        this.drawdaytue="#2F76D1";
-        this.drawdayfri="#AAAAAA";
-    }
-    fri(drawday){
-        this.credit_filter_draw=drawday;
-        this.credit_filter_day="Friday";
-        this.drawdayfri="#2F76D1";
-        this.drawdaytue="#AAAAAA";
-    }
-    sat(drawday){
-        this.fetch_filter_draw=drawday;
-        this.fetch_filter_day="Saturday";
-        this.drawdaywed="#AAAAAA";
-        this.drawdaysat="#FF0000";
-    }
-
-
-  credit_line(line){
-    this.credit_filter_line=parseInt(line);
-  }
-  fetch_line(line){
-    this.fetch_filter_line=parseInt(line);
-  }
+ watchSlider(line){
   
-  ionViewWillEnter() {
-    this.credit_filter_line=1;
-    this.credit_filter_draw="Tue";
-    this.credit_filter_day="Tuesday";
-    this.fetch_filter_line=1;
-    this.fetch_filter_draw="Wed";
-    this.fetch_filter_day="Wednesday";
+        //Converting slider-steps to custom values
 
-    this.authSrv.get_credit_offer().subscribe(data=>{
-      this.credit_lines=data.response.response.product[0];
-      this.credit_offer=data.response.response.offers;
-      console.log("get_credit_offer",data);
-    },
-    err=>{
-       console.log("error", err);
-      },
-    ()=> console.log("offer dislpay sucesss")
-  );
+     console.log(line);
+    }
 
-    this.authSrv.get_fetch_offer().subscribe(data=>{
-        this.fetch_lines=data.response.response.product[3];
-        this.fetch_offer=data.response.response.offers;
-        console.log("dd",data)
-      },
-      err=>{
-              console.log("error", err);
-      },
-      ()=> console.log("offer dislpay sucesss")
-    );
-
-    this.authSrv.get_Credit_Points().subscribe(data=>{
-        this.Credit_Points=data.response.response.bonus_credit;
-      },
-      err=>{          
-        console.log("error", err);
-      },
-      ()=> console.log("creadit points get successfully")
-    );
-
-  }
 }
-
-
