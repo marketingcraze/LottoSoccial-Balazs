f8eaa6aa400dc3ffdd7f2d4d98029e027ea56901
Dharam <dharam.enest@gmail.com>
2017-10-13 19:01:27 +0530

    Created new header and applies to all pages for < arrow Affiliate page bottom tab removal Update page integration Play games page booster image enlarging Get games page design amendments

--- src/pages/update/update.ts
+++ src/pages/update/update.ts
@@ -13,18 +13,31 @@ import { Market } from '@ionic-native/market';
 	selector: 'page-update',
 	templateUrl: 'update.html',
 })
-export class UpdatePage  {
- 
+export class UpdatePage {
+	tabbarElement: any;
+
 	//spaceBetween:number ;
 	constructor(
 		private market: Market,
 		public navCtrl: NavController) {
+		this.tabbarElement = document.querySelector('.tabbar');
 		//   this.spaceBetween = Math.floor( platform.width() * -0.14 );
 
 	}
 
-	updateNow(){
-		console.log("UpdatePage::updateNow ");
-		this.market.open('com.appv2.lottosocial');
+	updateNow() {
+		debugger;
+		this.tabbarElement.style.display = 'flex';
+		this.market.open('com.appv2.lottosocial').then((data) => {
+			console.log(data);
+		})
+		this.navCtrl.pop();
 	}
+	ionViewWillEnter() {
+		this.tabbarElement.style.display = 'none';
+	}
+	ionViewWillLeave() {
+		this.tabbarElement.style.display = 'flex';
+	}
+
 }
